f45834bc9307ce334cc469d85477af1b71b53244
OdinOxin <OdinOxin@googlemail.com>
2016-02-24 10:25:26 +0100

    Base Client

--- src/main/java/de/lebk/jwebpoll/client/Client.java
+++ src/main/java/de/lebk/jwebpoll/client/Client.java
@@ -1,9 +1,16 @@
 package de.lebk.jwebpoll.client;
 
 import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SplitPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.awt.*;
 
 public class Client extends Application
 {
@@ -12,12 +19,23 @@ public class Client extends Application
         Client.launch(args);
     }
 
+    private ListView<String> pollList;
+
     @Override
     public void start(Stage primaryStage) throws Exception
     {
         primaryStage.setTitle("JWebPoll");
 
-        GridPane root = new GridPane();
+        VBox root = (VBox) FXMLLoader.load(this.getClass().getResource("/client/client.fxml"));
+        SplitPane rootSplit = (SplitPane) root.lookup("#rootSplit");
+        this.pollList = new ListView<String>();
+        this.pollList.getItems().add("1. Umfrage");
+        this.pollList.getItems().add("Bundestagswahlen");
+        this.pollList.getItems().add("Mittag-Menü-Wahl");
+        rootSplit.getItems().add(this.pollList);
+
+        GridPane pollDetail = (GridPane) FXMLLoader.load(this.getClass().getResource("/client/pollDetail.fxml"));
+        rootSplit.getItems().add(pollDetail);
 
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
