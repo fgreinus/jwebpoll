fddb77e6ee147ac03937e77f07d22518e30feeec
OdinOxin <OdinOxin@googlemail.com>
2016-04-22 09:43:41 +0200

    Questions and Answers

--- src/main/java/de/lebk/jwebpoll/client/Client.java
+++ src/main/java/de/lebk/jwebpoll/client/Client.java
@@ -2,9 +2,10 @@ package de.lebk.jwebpoll.client;
 
 import de.lebk.jwebpoll.data.*;
 import javafx.application.Application;
-import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -35,7 +36,9 @@ public class Client extends Application
     private TextArea descTxF;
     private TextField createdDateTxF, createdTimeTxF;
     private ComboBox<PollState> stateCbo;
+    private Button openBtn, closeBtn, resultsBtn;
     private ListView<Question> questionList;
+    private Button questionsAddBtn, questionsRemoveBtn;
 
     @Override
     public void start(Stage primaryStage) throws Exception
@@ -45,9 +48,6 @@ public class Client extends Application
 
         //Default Poll: new poll
         Poll newPoll = new Poll("Neue Umfrage", "", PollState.NEW);
-        Question newQuestion = new Question("Neue Frage", true, QuestionType.SINGLE);
-        newQuestion.setHint("Hinweis zur Frage hier eingeben...");
-        newPoll.getQuestions().add(newQuestion);
         polls.add(newPoll);
 
         //Example polls (to be deleted in future)
@@ -69,7 +69,7 @@ public class Client extends Application
             Client.this.setPoll(newValue);
         });
         rootSplit.getItems().add(this.pollList);
-        rootSplit.setDividerPositions(1d / 4d);
+        rootSplit.setDividerPositions(1d / 5d);
 
         //PollView (Right side)
         ScrollPane pollDetailScroller = new ScrollPane();
@@ -101,16 +101,58 @@ public class Client extends Application
             return new PollStateCell();
         });
         this.stateCbo.setButtonCell(new PollStateCell());
-        this.questionList = (ListView<Question>) pollDetail.lookup("#questionList");
-        this.questionList.getSelectionModel().selectedItemProperty().addListener((ObservableValue<? extends Question> observable, Question oldValue, Question newValue) ->
+        this.openBtn = (Button) pollDetail.lookup("#openBtn");
+        this.openBtn.setOnAction((ActionEvent event) ->
         {
-            Platform.runLater(() -> Client.this.questionList.getSelectionModel().select(-1));
+            if(this.poll != null)
+            {
+                this.poll.setState(PollState.OPEN);
+                this.openBtn.setVisible(false);
+                this.closeBtn.setVisible(true);
+                this.stateCbo.setValue(this.poll.getState());
+            }
         });
+        this.closeBtn = (Button) pollDetail.lookup("#closeBtn");
+        this.closeBtn.setOnAction((ActionEvent event) ->
+        {
+            if(this.poll != null)
+            {
+                this.poll.setState(PollState.CLOESED);
+                this.closeBtn.setVisible(false);
+                this.openBtn.setVisible(true);
+                this.stateCbo.setValue(this.poll.getState());
+            }
+        });
+        this.resultsBtn = (Button) pollDetail.lookup("#resultsBtn");
+        this.resultsBtn.setOnAction((ActionEvent event) ->
+        {
+            //TODO View Results
+        });
+        this.questionList = (ListView<Question>) pollDetail.lookup("#questionList");
         this.questionList.setCellFactory((ListView<Question> param) ->
         {
             return new QuestionListCell();
         });
         this.pollList.getSelectionModel().selectFirst();
+        this.questionsAddBtn = (Button) pollDetail.lookup("#questionsAddBtn");
+        this.questionsAddBtn.setOnAction((ActionEvent event) ->
+        {
+            Question newQuestion = new Question("", true, QuestionType.SINGLE);
+            if(this.poll != null)
+                this.poll.getQuestions().add(newQuestion);
+            this.questionList.getItems().add(newQuestion);
+        });
+        this.questionsRemoveBtn = (Button) pollDetail.lookup("#questionsRemoveBtn");
+        this.questionsRemoveBtn.setOnAction((ActionEvent event) ->
+        {
+            if(!this.questionList.getSelectionModel().isEmpty())
+            {
+                Question toRemove = this.questionList.getSelectionModel().getSelectedItem();
+                if(this.poll != null)
+                    this.poll.getQuestions().remove(toRemove);
+                this.questionList.getItems().remove(toRemove);
+            }
+        });
         pollDetailScroller.setContent(pollDetail);
         rootSplit.getItems().add(pollDetailScroller);
 
@@ -132,6 +174,8 @@ public class Client extends Application
             this.createdDateTxF.setText(outputFormatDate.format(this.poll.getCreated()));
             this.createdTimeTxF.setText(outputFormatTime.format(this.poll.getCreated()));
             this.stateCbo.setValue(this.poll.getState());
+            this.openBtn.setVisible(this.poll.getState() == PollState.NEW || this.poll.getState() == PollState.CLOESED);
+            this.closeBtn.setVisible(this.poll.getState() == PollState.OPEN);
             this.questionList.getItems().clear();
             for (Question q : this.poll.getQuestions())
             {
@@ -163,23 +207,37 @@ public class Client extends Application
             {
                 try
                 {
-                    GridPane questionView = (GridPane) FXMLLoader.load(Client.this.getClass().getResource("/client/questionView.fxml"));
-                    Text numTxt = (Text) questionView.lookup("#numTxt");
-                    TextField titleTxt = (TextField) questionView.lookup("#titleTxF");
+                    GridPane rootGird = (GridPane) FXMLLoader.load(Client.this.getClass().getResource("/client/questionView.fxml"));
+                    GridPane questionView = (GridPane) rootGird.lookup("#questionGrid");
+                    Text titelTxt = (Text) questionView.lookup("#titelTxt");
+                    TextField titleTxF = (TextField) questionView.lookup("#titleTxF");
                     CheckBox requiredCkB = (CheckBox) questionView.lookup("#requiredCkB");
-                    TextField hintTxt = (TextField) questionView.lookup("#hintTxF");
+                    TextField hintTxF = (TextField) questionView.lookup("#hintTxF");
                     ComboBox<QuestionType> typeCbo = (ComboBox<QuestionType>) questionView.lookup("#typeCbo");
+                    Text answerAddTextTxt = (Text) questionView.lookup("#answerAddTextTxt");
                     TextField answerAddTextTxF = (TextField) questionView.lookup("#answerAddTextTxF");
+                    Text answerAddValueTxt = (Text) questionView.lookup("#answerAddValueTxt");
                     TextField answerAddValueTxF = (TextField) questionView.lookup("#answerAddValueTxF");
                     Button answerAddBtn = (Button) questionView.lookup("#answerAddBtn");
                     ListView<Answer> answerList = (ListView<Answer>) questionView.lookup("#answerList");
                     Button answerRemoveBtn = (Button) questionView.lookup("#answerRemoveBtn");
                     TextArea answerFreetext = (TextArea) questionView.lookup("#answerFreetext");
 
-                    numTxt.setText(String.format("%d.", 42));
-                    titleTxt.setText(item.getTitle());
+                    titleTxF.setText(item.getTitle());
+                    titleTxF.textProperty().addListener((ObservableValue<? extends String> observable, String oldValue, String newValue) ->
+                    {
+                        item.setTitle(titleTxF.getText());
+                    });
                     requiredCkB.setSelected(item.isRequired());
-                    hintTxt.setText(item.getHint());
+                    requiredCkB.setOnAction((ActionEvent event) ->
+                    {
+                        item.setRequired(requiredCkB.isSelected());
+                    });
+                    hintTxF.setText(item.getHint());
+                    hintTxF.textProperty().addListener((ObservableValue<? extends String> observable, String oldValue, String newValue) ->
+                    {
+                        item.setHint(hintTxF.getText());
+                    });
                     typeCbo.getItems().addAll(QuestionType.SINGLE, QuestionType.MULTIPLE, QuestionType.FREE);
                     typeCbo.setCellFactory((ListView<QuestionType> param) ->
                     {
@@ -193,7 +251,9 @@ public class Client extends Application
                         {
                             //TODO Implement different Views
                             case SINGLE:
+                                answerAddTextTxt.setVisible(true);
                                 answerAddTextTxF.setVisible(true);
+                                answerAddValueTxt.setVisible(true);
                                 answerAddValueTxF.setVisible(true);
                                 answerAddBtn.setVisible(true);
                                 answerList.setVisible(true);
@@ -201,7 +261,9 @@ public class Client extends Application
                                 answerFreetext.setVisible(false);
                                 break;
                             case MULTIPLE:
+                                answerAddTextTxt.setVisible(true);
                                 answerAddTextTxF.setVisible(true);
+                                answerAddValueTxt.setVisible(true);
                                 answerAddValueTxF.setVisible(true);
                                 answerAddBtn.setVisible(true);
                                 answerList.setVisible(true);
@@ -209,7 +271,9 @@ public class Client extends Application
                                 answerFreetext.setVisible(false);
                                 break;
                             case FREE:
+                                answerAddTextTxt.setVisible(false);
                                 answerAddTextTxF.setVisible(false);
+                                answerAddValueTxt.setVisible(false);
                                 answerAddValueTxF.setVisible(false);
                                 answerAddBtn.setVisible(false);
                                 answerList.setVisible(false);
@@ -253,8 +317,9 @@ public class Client extends Application
                             answerList.getItems().remove(toRemove);
                         }
                     });
+                    answerList.getItems().addAll(item.getAnswers());
 
-                    this.setGraphic(questionView);
+                    this.setGraphic(rootGird);
                 }
                 catch (IOException e)
                 {
@@ -298,9 +363,9 @@ public class Client extends Application
                             checkBox.setText(item.getText());
                             this.setGraphic(checkBox);
                             break;
-                        case FREE:
-                            TextArea textArea = (TextArea) FXMLLoader.load(Client.this.getClass().getResource("/client/answer_free.fxml"));
-                            this.setGraphic(textArea);
+                        default:
+                            this.setText(null);
+                            this.setGraphic(null);
                             break;
                     }
                 }
