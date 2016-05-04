1899c875042f5f8495daa635c3cb2d645c3faaec
OdinOxin <OdinOxin@googlemail.com>
2016-05-04 09:03:47 +0200

    Table views

--- src/main/java/de/lebk/jwebpoll/client/QuestionView.java
+++ src/main/java/de/lebk/jwebpoll/client/QuestionView.java
@@ -1,26 +1,28 @@
 package de.lebk.jwebpoll.client;
 
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 import de.lebk.jwebpoll.data.Answer;
 import de.lebk.jwebpoll.data.Question;
 import de.lebk.jwebpoll.data.QuestionType;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
+import javafx.util.Callback;
 
 import java.io.IOException;
 import java.io.ObjectInput;
+import java.io.StreamCorruptedException;
 
-public class QuestionView
-{
-    public static void setQuestionView(TitledPane parent, Question item, boolean disabled)
-    {
-        try
-        {
+public class QuestionView {
+    public static void setQuestionView(TitledPane parent, Question item, boolean disabled) {
+        try {
             GridPane rootGird = (GridPane) FXMLLoader.load(QuestionView.class.getResource("/client/questionView.fxml"));
             Text titelTxt = (Text) rootGird.lookup("#titelTxt");
             TextField titleTxF = (TextField) rootGird.lookup("#titleTxF");
@@ -41,7 +43,7 @@ public class QuestionView
             {
                 item.setTitle(titleTxF.getText());
 
-                if(item.getTitle() == null || item.getTitle().isEmpty())
+                if (item.getTitle() == null || item.getTitle().isEmpty())
                     parent.setText("<Neue Frage>");
                 else
                     parent.setText(item.getTitle());
@@ -69,8 +71,7 @@ public class QuestionView
             typeCbo.getSelectionModel().selectedItemProperty().addListener((ObservableValue<? extends QuestionType> observable, QuestionType oldValue, QuestionType newValue) ->
             {
                 item.setType(newValue);
-                switch(newValue)
-                {
+                switch (newValue) {
                     case SINGLE:
                         answerAddTextTxt.setVisible(true);
                         answerAddTextTxF.setVisible(true);
@@ -103,7 +104,38 @@ public class QuestionView
                         break;
                 }
 
-//                answerTable.getColumns().get(0).setCellFactory(TextFieldTableCell.forTableColumn());
+                TableColumn<Answer, QuestionType> typeColumn = (TableColumn<Answer, QuestionType>) answerTable.getColumns().get(0);
+                typeColumn.setCellValueFactory(new QuestionTypeTableCell(item.getType()));
+                typeColumn.setCellFactory(new Callback<TableColumn<Answer, QuestionType>, TableCell<Answer, QuestionType>>() {
+                    @Override
+                    public TableCell<Answer, QuestionType> call(TableColumn<Answer, QuestionType> btnCol) {
+                        return new TableCell<Answer, QuestionType>() {
+                            @Override
+                            public void updateItem(QuestionType type, boolean empty) {
+                                super.updateItem(type, empty);
+
+                                Node graphic = null;
+                                if (type != null && !empty) {
+                                    try {
+                                        switch (type) {
+                                            case SINGLE:
+                                                graphic = FXMLLoader.load(AnswerListCell.class.getResource("/client/answer_single.fxml"));
+                                                break;
+                                            case MULTIPLE:
+                                                graphic = FXMLLoader.load(AnswerListCell.class.getResource("/client/answer_multiple.fxml"));
+                                                break;
+                                        }
+                                    } catch (IOException ex) {
+
+                                    }
+                                } else {
+                                    setText(null);
+                                }
+                                setGraphic(graphic);
+                            }
+                        };
+                    }
+                });
             });
             typeCbo.setValue(item.getType());
             answerAddTextTxF.setDisable(disabled);
@@ -111,20 +143,16 @@ public class QuestionView
             answerAddValueTxF.setDisable(disabled);
             answerAddBtn.setOnAction((ActionEvent event) ->
             {
-                if(!answerAddTextTxF.getText().isEmpty()
-                        && !answerAddValueTxF.getText().isEmpty())
-                {
-                    try
-                    {
+                if (!answerAddTextTxF.getText().isEmpty()
+                        && !answerAddValueTxF.getText().isEmpty()) {
+                    try {
                         int value = Integer.parseInt(answerAddValueTxF.getText());
                         Answer answer = new Answer(answerAddTextTxF.getText(), value);
                         item.getAnswers().add(answer);
                         answerTable.getItems().add(answer);
                         answerAddTextTxF.clear();
                         QuestionView.updateAddValueTxF(item, answerAddValueTxF);
-                    }
-                    catch (NumberFormatException e)
-                    {
+                    } catch (NumberFormatException e) {
                         answerAddValueTxF.setText("Ungültiger Wert");
                     }
                 }
@@ -133,8 +161,7 @@ public class QuestionView
             answerAddBtn.setDisable(disabled);
             answerRemoveBtn.setOnAction((ActionEvent event) ->
             {
-                if(!answerTable.getSelectionModel().isEmpty())
-                {
+                if (!answerTable.getSelectionModel().isEmpty()) {
                     Answer toRemove = answerTable.getSelectionModel().getSelectedItem();
                     item.getAnswers().remove(toRemove);
                     answerTable.getItems().remove(toRemove);
@@ -142,27 +169,29 @@ public class QuestionView
                 }
             });
             answerRemoveBtn.setDisable(disabled);
+
+            TableColumn<Answer, String> textColumn = (TableColumn<Answer, String>) answerTable.getColumns().get(1);
+            textColumn.setCellValueFactory(new PropertyValueFactory<Answer, String>("text"));
+
+            TableColumn<Answer, String> valueColumn = (TableColumn<Answer, String>) answerTable.getColumns().get(2);
+            valueColumn.setCellValueFactory(new PropertyValueFactory<Answer, String>("value"));
             answerTable.getItems().addAll(item.getAnswers());
 
             parent.setContent(rootGird);
-        }
-        catch (IOException ex)
-        {
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
 
-    private static void updateAddValueTxF(Question item, TextField answerAddValueTxF)
-    {
-        if(item.getAnswers().isEmpty())
+    private static void updateAddValueTxF(Question item, TextField answerAddValueTxF) {
+        if (item.getAnswers().isEmpty())
             answerAddValueTxF.setText(String.valueOf(1));
-        else
-        {
+        else {
             int highest = Integer.MIN_VALUE;
             for (Answer answer : item.getAnswers())
-                if(answer.getValue() > highest)
+                if (answer.getValue() > highest)
                     highest = answer.getValue();
-            if(highest == Integer.MIN_VALUE)
+            if (highest == Integer.MIN_VALUE)
                 highest = 0;
             highest++;
             answerAddValueTxF.setText(String.valueOf(highest));
