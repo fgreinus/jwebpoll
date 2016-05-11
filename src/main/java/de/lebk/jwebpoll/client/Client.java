6185f2300beb11a8eabaabb9502dc7bd48836605
OdinOxin <OdinOxin@googlemail.com>
2016-05-11 09:42:09 +0200

    Poll add Button

--- src/main/java/de/lebk/jwebpoll/client/Client.java
+++ src/main/java/de/lebk/jwebpoll/client/Client.java
@@ -36,7 +36,7 @@ public class Client extends Application {
     private Poll activePoll;
 
     //- View -
-    private ListView<Poll> pollList = new ListView<>();
+    private ListView<Poll> pollList;
     private TextField titleTxF;
     private TextArea descTxF;
     private TextField createdDateTxF, createdTimeTxF;
@@ -97,7 +97,9 @@ public class Client extends Application {
         }
 
         //ListView (Left side)
-        SplitPane rootSplit = (SplitPane) FXMLLoader.load(this.getClass().getResource("/client/client.fxml"));
+        SplitPane rootSplit = FXMLLoader.load(this.getClass().getResource("/client/client.fxml"));
+        GridPane pollListView = FXMLLoader.load(this.getClass().getResource("/client/pollListView.fxml"));
+        this.pollList = (ListView<Poll>) pollListView.lookup("#pollList");
         this.pollList.setCellFactory((ListView<Poll> param) ->
         {
             return new PollListCell();
@@ -110,7 +112,7 @@ public class Client extends Application {
             if (newValue != null)
                 Client.this.setPoll(newValue);
         });
-        rootSplit.getItems().add(this.pollList);
+        rootSplit.getItems().add(pollListView);
         rootSplit.setDividerPositions(1d / 5d);
 
         //PollView (Right side)
