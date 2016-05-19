a6aba1bf53ac91e01ab81fd6888c86ad1036f4c5
LostInCoding <f.wosti@gmail.com>
2016-05-19 19:20:22 +0200

    First version is now running. It just shows the questions.

--- src/main/java/de/lebk/jwebpoll/client/EvaluationDialog.java
+++ src/main/java/de/lebk/jwebpoll/client/EvaluationDialog.java
@@ -2,6 +2,7 @@ package de.lebk.jwebpoll.client;
 
 import com.j256.ormlite.dao.ForeignCollection;
 import de.lebk.jwebpoll.data.Poll;
+import de.lebk.jwebpoll.data.Question;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -22,26 +23,21 @@ public class EvaluationDialog {
     public static void show(Poll poll) {
 
         Stage evaluationStage = new Stage(StageStyle.UTILITY);
-        evaluationStage.setTitle("Auswertung: "+poll.getTitle());
+        evaluationStage.setTitle("Auswertung: " + poll.getTitle());
 
         GridPane evaluationGrid;
         try {
             evaluationGrid = FXMLLoader.load(ConfirmDialog.class.getResource("/client/evaluationDialog.fxml"));
             Accordion questionsAccordion = (Accordion) evaluationGrid.lookup("#questionsAccordion");
-          /*  Label confirmMsg = (Label) confirmGrid.lookup("#confirmMsg");
-            confirmMsg.setText(msg);
-
-            Button okBtn = (Button) confirmGrid.lookup("#okBtn");
-            okBtn.setOnAction((ActionEvent ev) ->
-            {
-                callback.confirm();
-                confirmStage.close();
-            });*/
+            for (Question item : poll.getQuestions()) {
+                EvaluationQuestionView.setQuestionView(questionsAccordion, item, false);
+            }
+            evaluationGrid.setVisible(true);
+            questionsAccordion.setVisible(true);
         } catch (IOException ex) {
 
             return;
         }
-
 
 
         evaluationStage.setScene(new Scene(evaluationGrid));
