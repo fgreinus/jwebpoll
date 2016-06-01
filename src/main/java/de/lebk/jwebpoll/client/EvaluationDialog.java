e4bc091a90da7ce86333e3d24c204d6867a366ca
SimonMilan <solano.oktave+github@gmail.com>
2016-06-01 13:37:06 +0200

    Fixed the calculation of the variance and the standard deviation.

--- src/main/java/de/lebk/jwebpoll/client/EvaluationDialog.java
+++ src/main/java/de/lebk/jwebpoll/client/EvaluationDialog.java
@@ -1,5 +1,6 @@
 package de.lebk.jwebpoll.client;
 
+import com.j256.ormlite.dao.ForeignCollection;
 import de.lebk.jwebpoll.data.Answer;
 import de.lebk.jwebpoll.data.Poll;
 import de.lebk.jwebpoll.data.Question;
@@ -11,6 +12,7 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by lostincoding on 18.05.16.
@@ -25,12 +27,34 @@ public class EvaluationDialog {
         try {
             evaluationGrid = FXMLLoader.load(ConfirmDialog.class.getResource("/client/evaluationDialog.fxml"));
             Accordion questionsAccordion = (Accordion) evaluationGrid.lookup("#questionsAccordion");
+
+            int voteCountTotal = 0;
             for (Question question : poll.questions) {
                 EvaluationQuestionView.setQuestionView(questionsAccordion, question, false);
+
                 for (Answer answer : question.getAnswers()) {
-                    int voteCount = answer.getVotes().size();
-                    System.out.println("  " + answer.getText() + ": " + voteCount + " (weighted: " + voteCount * answer.getValue() + ")");
+                    voteCountTotal += answer.getVotes().size();
                 }
+
+                int weightedPollTotal = 0;
+                int weightedPollCount = 0;
+                for (Answer answer : question.getAnswers()) {
+                    weightedPollTotal += answer.getVotes().size() * answer.getValue();
+                    if (weightedPollTotal != 0) {
+                        weightedPollCount += answer.getVotes().size();
+                    }
+                    //double average = getAverage(weightedPollVal, voteCountTotal);
+
+                    //System.out.println("  " + answer.getText() + ": " + weightedPollVal + " (weighted: " + weightedPollVal * answer.getValue() + ")");
+
+                    //" | " + " arithmetic avg.: " + average + " | " + " standard deviation: " + getStandardDeviation(question.getAnswers(), average) +
+                }
+                double arithmeticAverage = (double)weightedPollTotal/(double)weightedPollCount;
+                System.out.println("Arithmetic average: " + arithmeticAverage);
+                double variance = getVarianz(question.getAnswers(), arithmeticAverage);
+                System.out.println("Variance: " + variance);
+                double standardDeviation = getStandardDeviation(question.getAnswers(), arithmeticAverage);
+                System.out.println("Standard deviation: " + standardDeviation);
             }
             evaluationGrid.setVisible(true);
             questionsAccordion.setVisible(true);
@@ -39,9 +63,33 @@ public class EvaluationDialog {
             return;
         }
 
-
         evaluationStage.setScene(new Scene(evaluationGrid));
         evaluationStage.sizeToScene();
         evaluationStage.show();
+    }
+
+    public static double getAverage(int voteCount, int voteCountTotal) {
+        return (double)voteCount/(double)voteCountTotal;
+    }
+
+    public static double getStandardDeviation(ForeignCollection<Answer> answers, double average) {
+        return Math.sqrt(getVarianz(answers, average));
+    }
+
+    public static double getVarianz(ForeignCollection<Answer> answers, double average) {
+        double voteCountTotal = 0;
+        for (Answer answer : answers) {
+            voteCountTotal += (double)answer.getVotes().size();
+        }
+        double sum = 0.0;
+        for (Answer answer : answers) {
+            if (answer.getVotes().size() > 0) {
+                double answerValue = (double)answer.getValue();
+                double termA = answerValue - average;
+                double varianceElement = Math.pow(termA, 2);
+                sum += varianceElement * (double)answer.getVotes().size();
+            }
+        }
+        return (double)sum / (double)voteCountTotal;
     }
 }
