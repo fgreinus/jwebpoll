1adc4a412c6c369982828de94fa6577de9cd9ee0
OdinOxin <OdinOxin@googlemail.com>
2016-06-10 10:20:09 +0200

    Added AVG-Line

--- src/main/java/de/lebk/jwebpoll/client/Evaluation/EvaluationQuestionView.java
+++ src/main/java/de/lebk/jwebpoll/client/Evaluation/EvaluationQuestionView.java
@@ -19,6 +19,8 @@ import java.util.Comparator;
 
 public class EvaluationQuestionView {
     private static final Logger LOGGER = Logger.getLogger(EvaluationQuestionView.class);
+    private static final String SUM = "Summe";
+    private static final String AVG = "Durchschnitt";
 
     public static void setQuestionView(Accordion accordion, Question question) {
         if (accordion == null)
@@ -64,37 +66,70 @@ public class EvaluationQuestionView {
     private static void fillForSingleAndMultipleChoice(Question question, TableView<Answer> answerTable) {
         int sumCount = 0;
         int sumWeight = 0;
+        double avgCount = 0;
+        double avgWeight = 0;
         if (question.getAnswers() != null) {
             answerTable.getItems().addAll(question.getAnswers());
             for (Answer answer : question.getAnswers()) {
                 sumCount += answer.getVotes().size();
                 sumWeight += answer.getVotes().size() * answer.getValue();
             }
-            answerTable.getItems().add(new Answer("Summe", sumCount, null));
+            avgCount = (double) sumCount / question.getAnswers().size();
+            avgWeight = (double) sumWeight / question.getAnswers().size();
+            answerTable.getItems().add(new Answer(SUM, sumCount, null));
+            answerTable.getItems().add(new Answer(AVG, (int) avgCount, null));
         }
         final int sumCountFinal = sumCount;
         final int sumWeightFinal = sumWeight;
+        final double avgCountFinal = avgCount;
+        final double avgWeightFinal = avgWeight;
 
         TableColumn<Answer, String> typeColumn = (TableColumn<Answer, String>) answerTable.getColumns().get(0);
         typeColumn.setCellValueFactory(new PropertyValueFactory<>("text"));
         typeColumn.prefWidthProperty().bind(answerTable.widthProperty().multiply(0.5));
 
         TableColumn<Answer, String> countColumn = new TableColumn<>("Häufigkeit");
-        countColumn.setCellValueFactory(cellData -> new SimpleStringProperty(String.valueOf(cellData.getValue().getQuestion() == null ? sumCountFinal : cellData.getValue().getVotes().size())));
+        countColumn.setCellValueFactory(cellData ->
+        {
+            Answer answer = cellData.getValue();
+            if (answer.getQuestion() == null) {
+                if (answer.getText().equals(SUM))
+                    return new SimpleStringProperty(String.valueOf(sumCountFinal));
+                if (answer.getText().equals(AVG))
+                    return new SimpleStringProperty(String.valueOf(avgCountFinal));
+            }
+            return new SimpleStringProperty(String.valueOf(answer.getVotes().size()));
+        });
         countColumn.prefWidthProperty().bind(answerTable.widthProperty().multiply(0.25));
 
         TableColumn<Answer, String> weightedColumn = new TableColumn<>("Gewichtet");
-        weightedColumn.setCellValueFactory(cellData -> new SimpleStringProperty(String.valueOf(cellData.getValue().getQuestion() == null ? sumWeightFinal : cellData.getValue().getVotes().size() * cellData.getValue().getValue())));
+        weightedColumn.setCellValueFactory(cellData ->
+        {
+            Answer answer = cellData.getValue();
+            if (answer.getQuestion() == null)
+            {
+                if(answer.getText().equals(SUM))
+                    return new SimpleStringProperty(String.valueOf(sumWeightFinal));
+                if(answer.getText().equals(AVG))
+                    return new SimpleStringProperty(String.valueOf(avgWeightFinal));
+            }
+            return new SimpleStringProperty(String.valueOf(answer.getVotes().size() * answer.getValue()));
+        });
         weightedColumn.prefWidthProperty().bind(answerTable.widthProperty().multiply(0.25));
 
         answerTable.setRowFactory(param ->
                 new TableRow<Answer>() {
                     @Override
-                    protected void updateItem(Answer item, boolean empty) {
-                        super.updateItem(item, empty);
+                    protected void updateItem(Answer answer, boolean empty) {
+                        super.updateItem(answer, empty);
 
-                        if (item != null && item.getQuestion() == null)
-                            setStyle("-fx-font-weight: bold;");
+                        if (answer != null && answer.getQuestion() == null)
+                        {
+                            if(answer.getText().equals(SUM))
+                                setStyle("-fx-font-weight: bold;");
+                            else if(answer.getText().equals(AVG))
+                                setStyle("-fx-font-style: italic;");
+                        }
                         else
                             setStyle("");
                     }
@@ -104,9 +139,17 @@ public class EvaluationQuestionView {
             Comparator<Answer> comparator = (a1, a2) ->
             {
                 if (a1.getQuestion() == null)
+                {
+                    if(a2.getQuestion() != null)
+                        return 1;
+                    if(a1.getText().equals(SUM))
+                        return -1;
                     return 1;
+                }
                 if (a2.getQuestion() == null)
+                {
                     return -1;
+                }
                 if (param.getComparator() == null)
                     return 0;
                 return param.getComparator().compare(a1, a2);
